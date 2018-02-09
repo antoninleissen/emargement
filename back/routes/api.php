cc9c12b4741d7a1248ba9bec4a98242cd900f4c6
antonin <aleissen@beable.fr>
2018-02-09 12:00:53 +0100

    initial commit

--- back/routes/api.php
+++ back/routes/api.php
@@ -13,22 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => array('api', 'auth:api')], function () {
+Route::group(['middleware' => []], function () {
 
     // USER SECTION
     Route::get('account/whoAmI', 'Api\AccountController@getWhoAmI');
-    Route::get('pac/listByOwner', 'Api\PacController@listByOwner');
+//    Route::get('pac/listByOwner', 'Api\PacController@listByOwner');
     Route::get('account/getAccount', 'Api\AccountController@getAccount');
-    Route::get('intervention/getInterventionsByTechnician', 'Api\InterventionController@getInterventionsByTechnician');
-    Route::get('maintenance/getMaintenancesByCompany', 'Api\MaintenanceController@getMaintenancesByCompany');
-    Route::get('technician/getTechniciansByCompany', 'Api\TechnicianController@getTechniciansByCompany');
-
     Route::resource('account', 'Api\AccountController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
-    Route::resource('admin', 'Api\AdminController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
-    Route::resource('company', 'Api\CompanyController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
-    Route::resource('customer', 'Api\CustomerController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
-    Route::resource('intervention', 'Api\InterventionController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
-    Route::resource('maintenance', 'Api\MaintenanceController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
-    Route::resource('pac', 'Api\PacController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
-    Route::resource('technician', 'Api\TechnicianController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
+    Route::resource('test', 'Api\TestController', ['only' => ['index', 'show', 'update', 'store', 'destroy']]);
 });
